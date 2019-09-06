a71ce232d4f18335fed89ebf3b52cd409cc86fac
Dariel Abreu <odalisdabreu@gmail.com>
2019-09-06 17:03:38 -0400

    menu 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'header', component: HeaderComponent},
-  {path: 'carousel', component: CarouselComponent}
+  {path: 'carousel', component: CarouselComponent},
+  {path: 'menu', component: MenuComponent}
 ];
 
 @NgModule({
